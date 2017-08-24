d1cae07dde16b4f0bcf9992d43cf112db847aa68
Igor Šarčević <igor@renderedtext.com>
2017-08-24 15:15:53 +0200

    Refactor subnamespace help

--- lib/dracula/namespace_help.rb
+++ lib/dracula/namespace_help.rb
@@ -8,46 +8,55 @@ class Dracula
     end
 
     def show
+      show_usage
+      show_desc if namespace.description
+      show_commands
+    end
+
+    private
+
+    def show_usage
       puts "Usage: #{Dracula.program_name} #{Dracula::UI.bold "#{namespace.prefix}[command]"}\n"
       puts ""
+    end
 
-      if namespace.description
-        puts namespace.description.capitalize
-        puts ""
-      end
+    def show_desc
+      puts namespace.description.capitalize
+      puts ""
+    end
 
+    def show_commands
       puts "Command list, type #{Dracula::UI.bold(Dracula.program_name.to_s + " help #{namespace.prefix}[command]")} for more details:"
       puts ""
 
       banners = []
 
-      namespace.commands.each do |cmd|
-        banners << [Dracula::UI.bold("#{namespace.prefix}#{cmd.desc.name}"), cmd.desc.description.capitalize]
-      end
-
+      banners += command_banners(namespace)
       banners << ["", ""] # empty line
 
       if namespace.top_level?
-        # show only namespaces
-
-        namespace.subcommands.each do |sub_cmd|
-          banners << [Dracula::UI.bold("#{namespace.prefix}#{sub_cmd.name}"), sub_cmd.description.capitalize]
-        end
-
+        banners += short_subcommand_banners(namespace)
         banners << ["", ""] # empty line
       else
-        # show namespaces with commands
-
         namespace.subcommands.each do |sub_cmd|
-          sub_cmd.commands.each do |cmd|
-            banners << [Dracula::UI.bold("#{sub_cmd.prefix}#{cmd.desc.name}"), cmd.desc.description.capitalize]
-          end
-
+          banners += command_banners(sub_cmd)
           banners << ["", ""] # empty line
         end
       end
 
       Dracula::UI.print_table(banners, :indent => 2)
+    end
+
+    def short_subcommand_banners(namespace)
+      namespace.subcommands.map do |sub_cmd|
+        [Dracula::UI.bold("#{namespace.prefix}#{sub_cmd.name}"), sub_cmd.description.capitalize]
+      end
+    end
+
+    def command_banners(namespace)
+      namespace.commands.map do |cmd|
+        [Dracula::UI.bold("#{namespace.prefix}#{cmd.desc.name}"), cmd.desc.description.capitalize]
+      end
     end
 
   end
